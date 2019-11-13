3c39167603ecb0fd693048248463260470330abf
TheAimMeister <57718263+TheAimMeister@users.noreply.github.com>
2019-11-13 17:11:38 +0100

    Add files via upload

--- ChangeFontSource.py
+++ ChangeFontSource.py
@@ -1,39 +1,51 @@
-import os
-import shutil
+import os, shutil, sys, time, psutil
 from os import listdir, walk
 
-def CustomFontDict():
-    onlyfiles = [f for f in listdir(os.getcwd()) if f.endswith('.ttf')]
-    return onlyfiles
+pid = str(os.getpid())
+pidfile = "ChangeFont.pid"
+if os.path.isfile(pidfile):
+    print(f"{pidfile} already exists, exiting")
+    time.sleep(2)
+    sys.exit(1)
 
-def GetRobloxVersion():
-    rootDict = f'C:/Users/{os.getlogin()}/AppData/Local/Roblox/Versions'
-    for (dirpath, dirnames, filenames) in walk(rootDict):
-        if "RobloxPlayerBeta.exe" in filenames:
-            rootDict = dirpath
-            print(f'Got Roblox Version at: {dirpath}')
-            break
-    return rootDict
+open(pidfile, 'w').write(pid)
 
-def GetFonts():
-    rootDict = f'{GetRobloxVersion()}/content/fonts'
-    fonts = [f for f in listdir(rootDict) if f.endswith('.ttf')]
-    print('Creating backup of original fonts.')
-    if not os.path.isdir(f'{os.getcwd()}/OriginalRobloxFont.bak'):
-        os.mkdir('OriginalRobloxFont.bak')
-    for i in fonts:
-        print(f'Creating backup of font: {i}')
-        shutil.copy(f'{rootDict}/{i}', f'{os.getcwd()}/OriginalRobloxFont.bak')
-    return fonts, rootDict
+try:
+    def CustomFontDict():
+        onlyfiles = [f for f in listdir(os.getcwd()) if f.endswith('.ttf')]
+        return onlyfiles
 
-def ChangeFontsPF():
-    CustomFont = CustomFontDict()
-    RobloxFont, rootDict = GetFonts()
-    for i in RobloxFont:
-        if i in CustomFont:
-            print(f'Replacing font: {i}')
-            shutil.copy(f'{os.getcwd()}/{i}', f'{rootDict}')
+    def GetRobloxVersion():
+        rootDict = f'C:/Users/{os.getlogin()}/AppData/Local/Roblox/Versions'
+        for (dirpath, dirnames, filenames) in walk(rootDict):
+            if "RobloxPlayerBeta.exe" in filenames:
+                rootDict = dirpath
+                print(f'Got Roblox Version at: {dirpath}')
+                break
+        return rootDict
 
-ChangeFontsPF()
-print('Operation done, press "return" to close')
-input()
+    def GetFonts():
+        rootDict = f'{GetRobloxVersion()}/content/fonts'
+        fonts = [f for f in listdir(rootDict) if f.endswith('.ttf')]
+        print('Creating backup of original fonts.')
+        if not os.path.isdir(f'{os.getcwd()}/OriginalRobloxFont.bak'):
+            os.mkdir('OriginalRobloxFont.bak')
+        for i in fonts:
+            print(f'Creating backup of font: {i}')
+            shutil.copy(f'{rootDict}/{i}', f'{os.getcwd()}/OriginalRobloxFont.bak')
+        return fonts, rootDict
+
+    def ChangeFontsPF():
+        CustomFont = CustomFontDict()
+        RobloxFont, rootDict = GetFonts()
+        for i in RobloxFont:
+            if i in CustomFont:
+                print(f'Replacing font: {i}')
+                shutil.copy(f'{os.getcwd()}/{i}', f'{rootDict}')
+
+    ChangeFontsPF()
+    print('Operation done, press "return" to close')
+    input()
+finally:
+    os.unlink(pidfile)
+    sys.exit(0)
