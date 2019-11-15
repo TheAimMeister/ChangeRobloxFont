5069f6cc54646a5dad4b808cc71ff0814a2b2223
TheAimMeister <57718263+TheAimMeister@users.noreply.github.com>
2019-11-15 22:59:51 +0100

    Add files via upload

--- ChangeFontSource.py
+++ ChangeFontSource.py
@@ -8,7 +8,8 @@ if os.path.isfile(pidfile):
     time.sleep(2)
     sys.exit(1)
 
-open(pidfile, 'w').write(str(os.getpid()))
+with open(pidfile, 'w') as e:
+    e.write(str(os.getpid()))
 
 #Mode definition
 PrStr = """
@@ -18,6 +19,7 @@ All      | Replace all fonts, by duplicating the custom fonts to
            the supported fonts (custom font file must be named 'arial.ttf' without quotes).
            If you want multiple fonts, please put them with the correct file name in CustomFont.bak
 Restore  | Restore Roblox's default font.
+Exit     | Exit the program.
 
 > """
 
@@ -25,7 +27,7 @@ def requestMode():
     os.system('cls' if os.name == 'nt' else 'clear')
     while True:
         mode = input(PrStr)
-        if mode.lower() in ["standard", "all", "restore"]:
+        if mode.lower() in ["standard", "all", "restore", "exit"]:
             break
         else:
             print('Enter a valid mode, refreshing screen.')
@@ -36,14 +38,15 @@ def requestMode():
 #actual code
 try:
     def CustomFontDict():
-        if not os.path.isdir(f'{os.getcwd()}/CustomFont.bak'):
-            os.mkdir('CustomFont.bak')
         onlyfiles = [f for f in listdir(os.getcwd()) if f.endswith('.ttf') or f.endswith('.otf')]
+        if not os.path.isdir(f'{os.getcwd()}/CustomFont.bak'): os.mkdir('CustomFont.bak')
         if onlyfiles:
             for i in onlyfiles:
                 print(f'Moving custom font "{i}" to CustomFont.bak')
                 shutil.copy(f'{os.getcwd()}/{i}', f'{os.getcwd()}/CustomFont.bak')
                 os.unlink(i)
+            else:
+                print('Done moving custom font to CustomFont.bak')
 
     def GetRobloxVersion(mode=False):
         rootDict = f'{os.getenv("LOCALAPPDATA")}/Roblox/Versions'
@@ -55,16 +58,14 @@ try:
                 else:
                     rootDict += '/content/fonts'
                     break
-        if not mode:
-            print(f'Found Roblox version at:\n{rootDict}')
+        if not mode: print(f'Found Roblox version at:\n{rootDict}')
         return rootDict
 
     def GetFonts():
         rootDict = f'{GetRobloxVersion()}/content/fonts'
         fonts = [f for f in listdir(rootDict) if f.endswith('.ttf') or f.endswith('.otf')]
         print('Creating backup of original fonts.')
-        if not os.path.isdir(f'{os.getcwd()}/OriginalRobloxFont.bak'):
-            os.mkdir('OriginalRobloxFont.bak')
+        if not os.path.isdir(f'{os.getcwd()}/OriginalRobloxFont.bak'): os.mkdir('OriginalRobloxFont.bak')
         for i in fonts:
             if i in listdir(f'{os.getcwd()}/OriginalRobloxFont.bak'):
                 print(f'Skipping {i}, already backed up.')
@@ -74,7 +75,9 @@ try:
         return fonts, rootDict
 
     def ChangeFontsPF():
-        if modeReturn(requestMode()):
+        if modeReturn(requestMode()) == "exit":
+            return
+        else:
             CustomFont = [f for f in listdir(f'{os.getcwd()}/CustomFont.bak') if f.endswith('.ttf') or f.endswith('otf')]
             RobloxFont, rootDict = GetFonts()
             print("End of backup process")
@@ -82,6 +85,8 @@ try:
                 if i in CustomFont:
                     print(f'Replacing font: {i}')
                     shutil.copy(f'{os.getcwd()}/CustomFont.bak/{i}', f'{rootDict}')
+            else:
+                input('Operation done, press "return" to close.')
 
     def restoreMode():
         print('Initiated restore mode')
@@ -109,6 +114,8 @@ try:
             return True
         elif mode.lower() == 'all':
             response = allMode()
+        elif mode.lower() == 'exit':
+            response = mode
         else:
             response = restoreMode()
         return response
@@ -120,6 +127,5 @@ except OSError as e:
     os.unlink(pidfile)
     sys.exit(1)
 finally:
-    input('Operation done, press "return" to close.')
     os.unlink(pidfile)
     sys.exit(0)
